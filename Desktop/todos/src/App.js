3b1471134e920612a357cf47cc133b76e0bba089
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-11 15:30:42 -0500

    innit

--- Desktop/todos/src/App.js
+++ Desktop/todos/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
-import Todos from "./component/Todos";
+import "./App.css"
+import Todos from
 
 function App() {
-  return;
-  <div>
-    <Todos />
-  </div>;
+  return 
+  <div></div>;
 }
 
 export default App;
